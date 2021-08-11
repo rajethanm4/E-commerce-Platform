f38b139060ed35269058073cf05da0cc18ec2c4c
RAJ VARDHAN <81090051+rajethanm4@users.noreply.github.com>
2021-08-11 18:44:06 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php 
+require('top.php');
+?>
+<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner">
+                                <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.php">Home</a>
+                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                                  <span class="breadcrumb-item active">ABOUT US</span>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+        <!-- cart-main-area start -->
+        <div class="checkout-wrap ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="checkout__inner">
+                            <div class="accordion-list">
+                                <div class="accordion">
+								<p>This is our one of the important startup that is providung effiecient and value to the customer we started our company in2021 to achieve our goal our company provide fastest delivey to the customer our company is now listed in NSE and BSE with net wortth insreasing daily</p>
+								<p>Our customers are our top priority and we do not want to lose any of you. So please drive safe and never text while on the wheel. It is dangerous.</p>	
+								</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        						
+<?php require('footer.php')?>        
